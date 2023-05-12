f207e0a89d89e175ea139f1e43b09f91e7d70186
shweta <bawaneshweta1994@gmail.com>
2023-05-12 17:35:16 +0530

    my-second-commit

--- main.tf
+++ main.tf
@@ -1,24 +1,18 @@
 provider "aws" {
-  region = "us-east-2b"
+  region  = "us-west-2"
 }
 
-resource "aws_instance" "web" {
-    ami = "ami-083eed19fc801d7a4"
-    instance_type = "t2.micro"
-    associate_public_ip_address = true
-    key_name = "ohio-key"
-    security_groups = ["k8s-sg"]
-
-    ebs_block_device {
-      device_name = "/dev/sdf"
-      volume_size = 10
-      volume_type = "gp2"
-    }
-
-     tags = {
-    Name = "my-tf-server"
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  key_name = "ohio-key"
+  security_groups = [ "k8s-sg" ]
+  ebs_block_device {
+    volume_size = 10
+    volume_type = "gp2"
   }
 
-
-
+  tags = {
+    Name = "my-tf-server"
+  }
 }

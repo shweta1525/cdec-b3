51f8e9b23df4fc8a2f385a443248469db137c26e
shweta <bawaneshweta1994@gmail.com>
2023-05-12 17:54:04 +0530

    4

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "web" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   key_name = "ohio-key"
